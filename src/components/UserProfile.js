18259b2cc2ae854f9f256671e00b9d7bab4459c8
tamekia_nelson <tamekialashay@outlook.com>
2019-06-05 20:36:28 -0400

    add branch, changes to userprofile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,16 +1,84 @@
+// import React, { Component } from "react";
+// import Navbar from "./Navbar";
+
+// //TODO: decide what will be displayed
+
+// class UserProfile extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Navbar />
+//         <p>This is the user profile</p>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default UserProfile;
+
+
 import React, { Component } from "react";
-import Navbar from "./Navbar";
+// import Image, {Component} from "react";
 
 //TODO: decide what will be displayed
 
+// Tamekia - added something to start with to build the user profile page. 
+// So far the general idea is showing on screen just missing actual content 
+// to make it work... Some items require styling to make them line up the way
+// we would like the page to look
+
+
 class UserProfile extends Component {
+  state = {
+    // userInfo
+    username: " ", 
+    img: " ", 
+    bio: " ", 
+    // userBar
+    following: " ",
+    followers: " ",
+    // userContent
+    content: " ", 
+    likes: " ",
+    comments: " ", 
+    rekwit: " ",
+    // logo
+    kwitterLogo: " ", 
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <Navbar />
-        <p>This is the user profile</p>
-      </React.Fragment>
-    );
+    <div className = "userProfile">
+
+    <div className = "userBar">
+    <h5>Following: {this.state.following}</h5>
+    <h5>Followers: {this.state.followers}</h5>
+    </div>
+
+    <div className = "userInfo"> 
+    <p>This is the user profile</p>
+    <h1>{this.state.username}</h1>
+    <image src= {this.state.img}></image>
+    <h3>Bio: {this.state.bio}</h3>
+    </div>
+    
+    <div className = "userContent">
+    <p>Users post here... {this.state.content}</p>
+    <button> Load media here... </button>
+    <h5>Likes: {this.state.like}</h5>
+    <button>Click to Like</button>
+    <h5>Comments: {this.state.comments}</h5>
+    <button>Click to Comment</button>
+    <h5>Rekwits: {this.state.rekwit}</h5>
+    <button>Click to Rekwit</button>
+    </div>
+
+    <div className = "logo">
+    <image src = {this.state.kwitterLogo}></image>
+    </div>
+    
+    </div>
+    )
   }
 }
 
