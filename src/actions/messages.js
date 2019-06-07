27920a311b70673f99534434492917a7b726935c
Dennis Christenson <eleeteneko5@yahoo.com>
2019-06-07 11:25:43 -0400

    started to write action for posting new message

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -1,1 +1,17 @@
-//TODO: posting?
+import { domain, jsonHeaders, handleJsonResponse } from "./constants";
+import { push } from "connected-react-router";
+
+export const MESSAGE_POST = "MESSAGE_POST";
+export const MESSAGE_SUCCESS = "MESSAGE_SUCCESS";
+export const MESSAGE_FAIL = "MESSAGE_FAIL";
+
+const url = domain + "/messages";
+
+const postMessage = messageData => dispatch => {
+  dispatch({ type: MESSAGE_POST });
+  return fetch(url, {
+    method: "POST",
+    headers: jsonHeaders,
+    body: JSON.stringify(messageData)
+  });
+};
