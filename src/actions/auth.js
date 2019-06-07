aac2ea908d225f2807634b3a4da293110ae0d173
Marsha Williamson <tramar92@hotmail.com>
2019-06-07 14:41:05 -0400

    logout working

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,6 +7,9 @@ import { push } from "connected-react-router";
 export const LOGIN = "LOGIN";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const LOGOUT_FAIL = "LOGOUT_FAIL";
 
 const url = domain + "/auth";
 
@@ -37,4 +40,33 @@ const login = loginData => dispatch => {
 
 export const loginThenGoToUserProfile = loginData => dispatch => {
   return dispatch(login(loginData)).then(() => dispatch(push("/profile")));
+};
+
+const logout = () => (dispatch, getState) => {
+  dispatch({
+    type: LOGOUT
+  });
+
+  const token = getState().auth.login.token;
+
+  return fetch(url + "/logout", {
+    method: "GET",
+    headers: { ...jsonHeaders, Authorization: `Bearer ${token}` }
+  })
+    .then(handleJsonResponse)
+    .then(result => {
+      return dispatch({
+        type: LOGOUT_SUCCESS,
+        payload: result
+      });
+    })
+    .catch(err => {
+      return Promise.reject(
+        dispatch({ type: LOGOUT_FAIL, payload: err.message })
+      );
+    });
+};
+
+export const logoutThenGoToLanding = () => dispatch => {
+  return dispatch(logout()).then(() => dispatch(push("/")));
 };
